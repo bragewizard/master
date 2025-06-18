ca5a6c2479b2231ed0b2f419b35b9bddae89b4f6
Brage <bragewiseth@icloud.com>
2025-06-18 19:26:59 +0200

    small tweaks

--- sim/src/utils.zig
+++ sim/src/utils.zig
@@ -1,9 +1,11 @@
+const std = @import("std");
+
 pub fn argsort(comptime T: type, allocator: std.mem.Allocator, values: []const T) ![]usize {
     const len = values.len;
 
     // Step 1: Create an array of indices
     var indices = try allocator.alloc(usize, len);
-    for (indices) |_, i| {
+    for (indices) |i| {
         indices[i] = i;
     }
 
@@ -15,4 +17,43 @@ pub fn argsort(comptime T: type, allocator: std.mem.Allocator, values: []const T
     }.lessThan);
 
     return indices;
+}
+
+// TODO: optimize and use vector if it is faster (need to benchmark)
+fn convolutionH(image: []const u8, new_bytes: []u8) void {
+    const kernel: @Vector(7, u32) = .{ 6, 61, 242, 383, 242, 61, 6 };
+    for (0..(image.len - 7)) |i| {
+        const vec: @Vector(7, u16) = image[i..][0..7].*;
+        const conv = vec * kernel;
+        const res = @reduce(.Add, conv);
+        new_bytes[i] = @intCast(res >> 10);
+    }
+}
+
+fn convolutionV(image: []const u8, width: usize, new_bytes: []u8) void {
+    const kernel: @Vector(7, u32) = .{ 6, 61, 242, 383, 242, 61, 6 };
+    for (0..(image.len - 7)) |i| {
+        const vec: @Vector(7, u16) = .{
+            image[0],
+            image[width],
+            image[width * 1],
+            image[width * 2],
+            image[width * 3],
+            image[width * 4],
+            image[width * 5],
+        };
+        const conv = vec * kernel;
+        const res = @reduce(.Add, conv);
+        new_bytes[i] = @intCast(res >> 10);
+    }
+}
+
+fn decimation(image: []const u8, width: usize, new_bytes: []u8) void {
+    var index: u32 = 0;
+    for (image, 0..) |pixel, i| {
+        if (i % 2 == 0 and (i / width) % 2 == 0) {
+            new_bytes[index] = pixel;
+            index += 1;
+        }
+    }
 }
